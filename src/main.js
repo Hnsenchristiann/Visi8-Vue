0900f7e4070b8be5d388654f60de4627813a5c62
Hnsenchristiann <christiansensen00@gmail.com>
2021-12-03 12:15:42 +0700

    apex charts realtime

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import VueApexCharts from 'vue-apexcharts'
 
 Vue.config.productionTip = false
 
+Vue.use(VueApexCharts)
+
+Vue.component('apexchart', VueApexCharts)
+
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="chart" class="home">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+     <apex-chart />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import ApexChart from '../components/ApexChart.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    // HelloWorld
+    ApexChart
+  }
+}
+</script>
